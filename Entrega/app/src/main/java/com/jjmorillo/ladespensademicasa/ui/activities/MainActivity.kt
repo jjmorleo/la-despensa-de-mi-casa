073c5e1d557357415836885773d39d571027efd0
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-11 13:09:16 +0200

    Creado paquetre ui

--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/activities/MainActivity.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jjmorillo.ladespensademicasa
+package com.jjmorillo.ladespensademicasa.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jjmorillo.ladespensademicasa.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
