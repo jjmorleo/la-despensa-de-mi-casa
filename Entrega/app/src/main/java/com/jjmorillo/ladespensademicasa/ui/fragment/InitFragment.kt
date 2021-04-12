f5a216db9d9f6f559c420552233333049ec427c7
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-12 10:34:14 +0200

    Creada Fragment Init

---
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/InitFragment.kt
@@ -0,0 +1,31 @@
+package com.jjmorillo.ladespensademicasa.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jjmorillo.ladespensademicasa.R
+import com.jjmorillo.ladespensademicasa.databinding.FragmentInitBinding
+
+
+class InitFragment : Fragment() {
+
+    private var _binding: FragmentInitBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInitBinding.inflate(inflater, container, false)
+        val view = binding.root
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
