04c06c218799046195f087b7a8c97490d6abd90f
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-13 18:17:38 +0200

    Se ha añadido Firebase al proyecto

--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/LoginFragment.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/LoginFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.jjmorillo.ladespensademicasa.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val view = binding.root
+        auth = Firebase.auth
         // Inflate the layout for this fragment
         return view
     }
--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/RegistroFragment.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/RegistroFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.jjmorillo.ladespensademicasa.databinding.FragmentRegistroBinding
 
 
@@ -12,11 +15,14 @@ class RegistroFragment : Fragment() {
 
     private var _binding: FragmentRegistroBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRegistroBinding.inflate(inflater, container, false)
         val view = binding.root
+        auth = Firebase.auth
         // Inflate the layout for this fragment
         return view
     }
