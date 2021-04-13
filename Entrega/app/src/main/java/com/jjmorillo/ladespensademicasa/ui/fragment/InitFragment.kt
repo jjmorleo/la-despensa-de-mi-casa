1dc8436bf8d7862a541f509eab8ad974e24ea5b4
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-13 15:54:24 +0200

    Creado nav Host y nav Graph. Naviagation

--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/InitFragment.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/InitFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import com.jjmorillo.ladespensademicasa.R
 import com.jjmorillo.ladespensademicasa.databinding.FragmentInitBinding
 
 
@@ -13,18 +15,23 @@ class InitFragment : Fragment() {
     private var _binding: FragmentInitBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentInitBinding.inflate(inflater, container, false)
         val view = binding.root
         // Inflate the layout for this fragment
+        _binding!!.initMaterialButton.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.action_to_loginFragment)
+
+        }
         return view
     }
 
    /* override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-*/
+    }*/
+
 }
