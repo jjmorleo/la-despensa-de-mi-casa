8f38ddc41de216b9e875c02b9751f198f8264978
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-15 03:16:09 +0200

    Creada la clase Producto

--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/models/Producto.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/models/Producto.kt
@@ -1,3 +1,10 @@
 package com.jjmorillo.ladespensademicasa.models
 
-data class Producto(val nombre: String)
+data class Producto(
+    val nombre: String,
+    val marca: String,
+    val descripcion: String,
+    val imagen: String,
+    val precio_unidad: Double,
+    val precio: Double
+)
