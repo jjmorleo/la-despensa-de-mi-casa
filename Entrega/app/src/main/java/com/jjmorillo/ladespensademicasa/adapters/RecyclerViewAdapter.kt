a4926e40d3650f6ef16052ef469fdd7e73afa0d7
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-15 11:26:03 +0200

    Creada la clase ViewHolder

--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/adapters/RecyclerViewAdapter.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/adapters/RecyclerViewAdapter.kt
@@ -1,4 +1,30 @@
 package com.jjmorillo.ladespensademicasa.adapters
 
-class RecyclerViewAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jjmorillo.ladespensademicasa.databinding.ItemProductoRecyclerViewBinding
+import com.jjmorillo.ladespensademicasa.models.Producto
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder private constructor(val binding: ItemProductoRecyclerViewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun rellenarDatos(producto: Producto){
+
+            binding.itemProducto.Id.text= producto.id.toString()
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
