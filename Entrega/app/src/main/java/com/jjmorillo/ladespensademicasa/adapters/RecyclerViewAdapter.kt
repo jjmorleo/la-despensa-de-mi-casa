87a82e0f40661f815df39306dabaf84a91cf609a
Juan Jose Morillo <jjmorleo@gmail.com>
2021-04-15 20:24:24 +0200

    Creada lista de productos

--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/adapters/RecyclerViewAdapter.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/adapters/RecyclerViewAdapter.kt
@@ -1,30 +1,61 @@
 package com.jjmorillo.ladespensademicasa.adapters
 
+
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.jjmorillo.ladespensademicasa.databinding.ItemProductoRecyclerViewBinding
 import com.jjmorillo.ladespensademicasa.models.Producto
 
-class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter(val productos:List<Producto>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
 
     class ViewHolder private constructor(val binding: ItemProductoRecyclerViewBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun rellenarDatos(producto: Producto){
+        fun rellenarDatos(producto: Producto) {
 
-            binding.itemProducto.Id.text= producto.id.toString()
+            binding.itemProductoId.text = producto.id.toString()
+            binding.itemProductoNombre.text = producto.nombre
+            binding.itemProductoMarca.text = producto.marca
+            binding.itemProductoDescripcion.text = producto.descripcion
+            binding.itemProductoImagen.text = producto.imagen
+            binding.itemProductoPrecioUnidad.text = producto.precio_unidad.toString()
+            binding.itemProductoPrecio.text = producto.precio.toString()
+            binding.itemProductoId1.text = producto.id.toString()
+            binding.itemProductoNombre1.text = producto.nombre
+            binding.itemProductoMarca1.text = producto.marca
+            binding.itemProductoDescripcion1.text = producto.descripcion
+            binding.itemProductoImagen1.text = producto.imagen
+            binding.itemProductoPrecioUnidad1.text = producto.precio_unidad.toString()
+            binding.itemProductoPrecio1.text = producto.precio.toString()
+        }
+
+
+        //Se crea un constructor que seria un companion object que lo que haria seria crear un objeto de la clase
+        //El companion object es lo mismo que crear una funcion estatica en Java
+        companion object {
+            fun crearViewHolder(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemProductoRecyclerViewBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
         }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= ViewHolder.crearViewHolder(parent)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) = viewHolder.rellenarDatos(productos[position])
+    override fun getItemCount() = productos.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    //Esto seria en formato Java como si fuera un array
+    /* override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+         holder.rellenarDatos(productos[position])
+     }*/
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    /* override fun getItemCount(): Int {
+         return productos.size
+
+     }
+ */
+
 }
--- Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/ProductosFragment.kt
+++ Entrega/app/src/main/java/com/jjmorillo/ladespensademicasa/ui/fragment/ProductosFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jjmorillo.ladespensademicasa.R
+import com.jjmorillo.ladespensademicasa.adapters.RecyclerViewAdapter
 import com.jjmorillo.ladespensademicasa.databinding.FragmentProductosBinding
+import com.jjmorillo.ladespensademicasa.models.Producto
 
 
 class ProductosFragment : Fragment() {
@@ -19,8 +25,73 @@ class ProductosFragment : Fragment() {
         binding = FragmentProductosBinding.inflate(inflater, container, false)
         val view = binding!!.root
 
+        loadListView()
+        createRecyclerView()
+
         return view
     }
 
+    private fun loadListView() {
 
+
+        /*
+        createListView()
+        */
+        createRecyclerView()
+
+
+    }
+
+    private fun createRecyclerView() {
+        //apply es para el patron builder
+        val mAdapter = RecyclerViewAdapter(cargarProductos())
+        val recyclerView = binding!!.productosRecyclerViewFragment
+        recyclerView.apply {
+            //EL RECYCLERVIEW VA A SER UNA LISTA HORIZONTAL
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+            adapter = mAdapter
+        }
+    }
+
+    /*private fun createListView() {
+        //CONTEXT
+        //LAYOUT DONDE CARGAR LOS DATOS
+        //LOS DATOS
+        *//*val adapter = ArrayAdapter<Producto>(
+                requireContext()
+                        android . R . layout . simple_list_item
+                        cargarProductos()
+        )*//*
+        //val adapter = ListViewAdapter(requireContext(), R.layout.item_producto_list_view, cargarProductos())
+        //binding!!.productosRecyclerViewFragment.adapter = adapter
+
+
+    }*/
+
+    private fun cargarProductos(): List<Producto> {
+
+        val productos = mutableListOf<Producto>()
+
+
+        productos.add(Producto("galletas dinosaurios", "gullon", "galletas de cereales 250g", "galletas de formato", 1.55, 3.95))
+        productos.add(Producto("cereales chocolate", "kollbram", "cereales de chocolate con leche 500g", "cerealessss", 1.22, 2.85))
+        productos.add(Producto("cereale integrales sin gluten", "hacendado", "cereales sin gluten 450g ", "tetrtrtrtrt", 0.65, 1.58))
+        productos.add(Producto("galletas cookies", "gullon", "galletas de trigo con chocolate 250g", "tetrtrtrtrt", 0.85, 1.85))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+        productos.add(Producto("nnnnnaa", "gagagag", "gdgdgdgdgdg", "tetrtrtrtrt", 0.95, 1.18))
+
+
+
+        return productos
+
+
+    }
 }
